ea7c061d61968b1c4546e59754d286223077d359
tommady <tommady@users.noreply.github.com>
2022-01-14 22:56:01 +0800

    Change to use timers and update tinyhttp (#2)
    * fixing combine the format!(..) arguments with the outer format!(..) call
    
    * change to use time.rs instead of chrono
    
    * fix testcase test_task_total_balance_of_relayers to use BSC testnet bridge

--- src/tasks/network_functional.rs
+++ src/tasks/network_functional.rs
@@ -1,10 +1,9 @@
-use chrono::{DateTime, Utc};
-
 use crate::config::ExtraOpts;
 
 use anyhow::{bail, Context, Result};
 use prometheus::core::Number;
 use serde_json::Value;
+use time::{format_description::well_known::Rfc3339, OffsetDateTime};
 
 pub(crate) fn network_functional<N: Number>(addr: &str, _opts: &Option<ExtraOpts>) -> Result<N> {
     let data: Value = ureq::get(&format!("{}/status", addr))
@@ -18,15 +17,16 @@ pub(crate) fn network_functional<N: Number>(addr: &str, _opts: &Option<ExtraOpts
         bail!("latest_block_time is null")
     }
 
+    // format is like 2022-01-14T13:44:55.889015796Z UTC time
     let latest_block_time = match latest_block_time.as_str() {
         Some(v) => v,
         None => bail!("latest_block_time is not a str"),
     };
 
-    let latest_block_timestamp = DateTime::parse_from_rfc3339(latest_block_time)
+    let latest_block_timestamp = OffsetDateTime::parse(latest_block_time, &Rfc3339)
         .context("parse latest_block_time failed")?
-        .timestamp();
-    let cur_timestamp = Utc::now().naive_utc().timestamp();
+        .unix_timestamp();
+    let cur_timestamp = OffsetDateTime::now_utc().unix_timestamp();
 
     Ok(N::from_i64((cur_timestamp - latest_block_timestamp).abs()))
 }
--- src/tasks/total_balance_of_relayers.rs
+++ src/tasks/total_balance_of_relayers.rs
@@ -57,8 +57,7 @@ pub(crate) fn total_balance_of_relayers<N: Number>(
             "params":[
                 {
                     "data":format!(
-                        "0x9010d07ce2b7fb3b832174769106daebcfd6d1970523240dda11281102db9363b83b0dc4{}", 
-                        format!("{:064x}", i)), 
+                        "0x9010d07ce2b7fb3b832174769106daebcfd6d1970523240dda11281102db9363b83b0dc4{:064x}", i), 
                     "to":bridge_addr
                 },
                 "latest"
@@ -147,12 +146,10 @@ mod tests {
 
     #[test]
     fn test_task_total_balance_of_relayers() {
-        // we only enable this feature on dev qa01 net currently
-        // TODO: change to use prod mainnet when it is ready
         assert!(total_balance_of_relayers::<u64>(
-            "https://dev-qa01.dev.findora.org:8545",
+            "https://data-seed-prebsc-1-s1.binance.org:8545",
             &Some(ExtraOpts::BridgeAddress(
-                "0x26925046a09d9AEfe6903eae0aD090be06186Bd9".to_string()
+                "0xD609931ec1c7a7F6ad59A69fede03fB067Af997c".to_string()
             ))
         )
         .is_ok())
